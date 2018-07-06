35f8165fcd3a7d0a15e3771aa206d9b235819c4e
Spoalove <justinpu02@yahoo.com>
2018-07-06 22:18:13 +0800

    Initial Commit

---
+++ JumpRopeCounterTests/JumpRopeCounterTests.swift
@@ -0,0 +1,36 @@
+//
+//  JumpRopeCounterTests.swift
+//  JumpRopeCounterTests
+//
+//  Created by Justin Pu on 2018/7/6.
+//  Copyright © 2018 Justin Pu. All rights reserved.
+//
+
+import XCTest
+@testable import JumpRopeCounter
+
+class JumpRopeCounterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
